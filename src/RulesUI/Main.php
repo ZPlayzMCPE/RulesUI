93c8a050726df4128546b958c3ea99a8351876af
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-03-22 16:38:47 +0000

    Update Main.php

--- src/RulesUI/Main.php
+++ src/RulesUI/Main.php
@@ -38,13 +38,20 @@ class Main extends PluginBase implements Listener {
 					});
 					$form->setTitle("§6Void§bFactions§cPE §dRules");
 					$form->setContent("Please follow the rules.");
-					$form->adddropdown(TextFormat::BOLD . "§aPlease respect staff and dont §4hack!");	
-					$form->adddropdown(TextFormat::DARK_RED  . "Don't Swear and grief!");
-					$form->adddropdown(TextFormat::BLUE  . "STR Grinding is bannable!");
+					$form->adddropdown(TextFormat::BOLD . "§aDon’t abuse your power.");	
+					$form->adddropdown(TextFormat::DARK_RED  . "Don’t kick/ban/mute/warn players for no reason.");
+					$form->adddropdown(TextFormat::BLUE  . "No asking for promotions.");
 					$form->adddropdown(TextFormat::RED  . "Never ask for staff!");
-					$form->adddropdown(TextFormat::RED . "§aDon't advertise");
-					$form->adddropdown(TextFormat::RED . "§bDon't accuse staff of abuse unless actual proof is given.");
-					$form->adddropdown(TextFormat::RED . "§aDon't be racist.");
+					$form->adddropdown(TextFormat::RED . "§aDon’t keep asking me questions. Just help the players (if any, and if they want or need help) DO NOT IGNORE THEM.");
+					$form->adddropdown(TextFormat::RED . "§b Don’t grief the server.");
+					$form->adddropdown(TextFormat::RED . "§aNo abusing your faction power. (STR Grinding.)");
+					$form->adddropdown(TextFormat::RED . "§bBe patient. Don’t get too excited, as we want to stay and keep professional.");
+					$form->adddropdown(TextFormat::RED . "§cDon’t use any banned commands which cheat the game, like /give.");
+					$form->adddropdown(TextFormat::RED . "§dWhen finding Hackers, make sure to report proof and ban them. (More info will be in #ban-info.");
+					$form->adddropdown(TextFormat::RED . "§eDon't tp raid, so don't teleport to someone's base by using /tp and raiding it. As that isn't allowed.");
+					$form->adddropdown(TextFormat::RED . "§aDon't abuse /invsee and /pv admin. Only use them if they have an abusive item or bannable items / gear.");
+					$form->adddropdown(TextFormat::RED . "§bFollow the warn info in #warn-info. Follow the ban info in #ban-info. Follow the mute info in #mute-info. Follow the kick info in #kick-info. Follow the Invsee info in #invsee-info.");
+					$form->adddropdown(TextFormat::RED . "§cNo advertising.");
 					$form->sendToPlayer($sender);
 				}
 				else{
